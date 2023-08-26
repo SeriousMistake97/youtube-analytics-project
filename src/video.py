fc61f5e61660d7126fe367aaccd90779203d855c
Alexandr(Serious) <sanekzh01@gmail.com>
2023-08-26 13:46:55 +0500

    Added an exception block to the Video class

--- src/video.py
+++ src/video.py
@@ -7,18 +7,25 @@ API_KEY: str = os.getenv("YT_API_KEY")
 
 class Video:
     def __init__(self, video_id: str):
-        self.video_id = video_id
+        try:
+            self.video_id = video_id
 
-        request = Video.get_service().videos().list(
-            part='snippet,statistics,contentDetails,topicDetails',
-            id=video_id,
-            key=API_KEY
-        ).execute()
+            request = Video.get_service().videos().list(
+                part='snippet,statistics,contentDetails,topicDetails',
+                id=video_id,
+                key=API_KEY
+            ).execute()
 
-        self.name_video = request['items'][0]['snippet']['title']
-        self.url_video = f"https://www.youtube.com/watch?v={self.video_id}&ab_channel={request['items'][0]['snippet']['channelTitle']}"
-        self.view_count = request['items'][0]['statistics']['viewCount']
-        self.like_count = request['items'][0]['statistics']['likeCount']
+            self.name_video = request['items'][0]['snippet']['title']
+            self.url_video = f"https://www.youtube.com/watch?v={self.video_id}&ab_channel={request['items'][0]['snippet']['channelTitle']}"
+            self.view_count = request['items'][0]['statistics']['viewCount']
+            self.like_count = request['items'][0]['statistics']['likeCount']
+        except IndexError:
+            print(f"Video with id {video_id} not found")
+            self.name_video = None
+            self.url_video = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return self.name_video
